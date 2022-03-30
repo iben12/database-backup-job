69548fb90b5b743da3517941ea8ab1061a07daf3
iben12 <iben@iben.hu>
2022-03-30 15:54:35 +0200

    Configure MYSQL databases

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -4,7 +4,9 @@ set -e
 
 echo "Backing up MYSQL data"
 
-mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD --single-transaction --all-databases > db.sql
+if [ ! -z "$MYSQL_DATABASES" ];then 
+  mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD --single-transaction --databases $MYSQL_DATABASES > db.sql
+fi
 
 echo "Compressing backup data..."
 timestamp=`date +%Y-%m-%d_%H-%M`
